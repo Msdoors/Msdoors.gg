eb0dc9b94ed0e9489a2dd109bfc46319e8cecba9
MsDoors.gg <msdoors.gg@gmail.com>
2025-01-28 15:22:12 -0300

    ✅ [ PRESSURE SUPPORT ]

--- Scripts/Msdoors/executavel.lua
+++ Scripts/Msdoors/executavel.lua
@@ -36,7 +36,8 @@ local SUPPORTED_GAMES = {
     [189707] = "NaturalDisaster/places/game.lua",
     [12137249458] = "CampoFFA/ArmasFFA.lua",
     [5275822877] = "Carrinho%2Bcart-para-Giganoob/game.lua",
-    [9285238704] = "Raceclicker/game.lua"
+    [9285238704] = "Raceclicker/game.lua",
+    [12411473842] = "pressure/game.lua"
 }
 
 local function notify(title, message, tipo)
