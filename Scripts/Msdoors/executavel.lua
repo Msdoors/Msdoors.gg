7777cdf15386ba584ab27c89c68cba96d986b7b5
Sc-Rhyan57 <rhyanlus501@gmail.com>
2025-03-10 16:28:31 -0300

    Update executavel.lua

--- Scripts/Msdoors/executavel.lua
+++ Scripts/Msdoors/executavel.lua
@@ -90,9 +90,9 @@ end
 
 local SCRIPT_URL = "https://raw.githubusercontent.com/Sc-Rhyan57/Msdoors/refs/heads/main/Src/Loaders/"
 local SUPPORTED_GAMES = {
-    [6516141723] = "Doors/lobby.lua",
-    [6839171747] = "Doors/hotel.lua", 
-    [2440500124] = "Doors/hotel.lua",
+    [6516141723] = "Doors/lobby.lua", -- lobby
+    [6839171747] = "Doors/hotel.lua", -- FLOORS 2
+    [2440500124] = "Doors/hotel.lua", -- FLOORS 1
     [10549820578] = "Doors/Fools23.lua",
     [110258689672367] = "Doors/OldLobby.lua",
     [189707] = "NaturalDisaster/places/game.lua",
