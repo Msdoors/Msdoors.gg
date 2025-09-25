8ea24b135f5bdd5e42c1f93896ce110adbf5e55c
Sc-Rhyan57 <rhyanlus501@gmail.com>
2025-09-25 20:49:25 -0300

    Update info.lua

--- misc/main/tabs/info.lua
+++ misc/main/tabs/info.lua
@@ -13,7 +13,6 @@ function misctabs:AddMiscTab(Tab)
 	local EspecialCreditsJacob = GroupContributors:AddLabel('<font color="#00FF00">[Jacob]</font> | He is a great person and helped me with tips and optimizing some of the script functions.', true)
     local EspecialCreditsKardinCat = GroupContributors:AddLabel('<font color="#00FF00">[TheHunterSolo1]</font> | This guy made several changes to the Msdoors code and optimized it a lot, and also gave me several tips.', true)
     local EspecialCreditsG1gaBac0n = GroupContributors:AddLabel('<font color="#FF0000">[G1ga Bac0n]</font> | for a while he was my Tester and helped me find bugs in the script.', true)
-
     --[[ TRANSLATORS ]]--
    -- SOOON
 return misctabs
