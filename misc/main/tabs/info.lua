1d6d43d77da59694df31e75618ace4056e80dcec
Msdoors <msdoors.gg@gmail.com>
2025-10-23 17:15:42 -0300

    Add files via upload

---
+++ misc/main/tabs/info.lua
@@ -0,0 +1,46 @@
+local misctabs = {} -- SOGMA EZ
+
+-- CREDITS, TRANSLATORS, ETC...
+function misctabs:AddMiscTab(Tab)
+    local GroupCredits = Tab:AddLeftGroupbox(shared.translationapi:getTranslate("tab_Credits", "Desenvolvedores"))
+    local GroupContributors = Tab:AddLeftGroupbox(shared.translationapi:getTranslate("tab_Credits_Contributors", "Contribuidores"))
+    local GroupTranslators = Tab:AddRightGroupbox(shared.translationapi:getTranslate("tab_Credits_Translators", "Tradutores"))
+
+    --[[ DEVS ]]--
+    local DevsCreditsRhyan = GroupCredits:AddLabel('[Rhyan57] | OWNER ', true)
+    local DevsCreditsSeek = GroupCredits:AddLabel('[SeekAlegriaFla] | SUB-OWNER ', true)
+
+    --[[ CONTRIBUTORS ]]--
+    local EspecialCreditsSeikoso = GroupContributors:AddLabel('[Seikoso] | It helped me with tips and to not give up on this script.', true)
+    local EspecialCreditsJacob = GroupContributors:AddLabel('[Jacob] | He is a great person and helped me with tips and optimizing some of the script functions.', true)
+    local EspecialCreditsKardinCat = GroupContributors:AddLabel('[TheHunterSolo1] | This guy made several changes to the Msdoors code and optimized it a lot, and also gave me several tips.', true)
+    local EspecialCreditsG1gaBac0n = GroupContributors:AddLabel('[G1ga Bac0n] | for a while he was my Tester and helped me find bugs in the script.', true)
+
+    --[[ TRANSLATORS ]]--
+    -- SOOON
+end
+
+-- SUMMARY UPDATE: DOORS
+function misctabs:AddWarnTabDoors(Tab)
+    Tab:UpdateWarningBox({
+        Title = shared.translationapi:getTranslate("Update Summary", "Resumo de Atualizações"),
+        Icon = "layout-grid",
+        Text = "[ √ ] fixed issue with not notifying entities\n[ + ] Death Farm\n[ ✓ ] FIXED CRASH ON DEATH\n[ > ] IMPROVED SPEED BYPASS\n» join our Discord to suggest features! «",
+        IsNormal = true,
+        Visible = true,
+        LockSize = true,
+    })
+end
+-- SUMMARY UPDATE: NATURAL DISASTER
+function misctabs:AddWarnTabNaturalDisaster(Tab)
+    Tab:UpdateWarningBox({
+        Title = shared.translationapi:getTranslate("Update Summary", "Resumo de Atualizações"),
+        Icon = "layout-grid",
+        Text = "[ ✓ ] FIXED NOTIFY DISASTERS\n[ ✓ ] FIXED ORBITAL PARTS\n[ ✓ ] FIXED TARGET PLAYERS\n[ ✓ ] OPTIMIZED\n» join our Discord to suggest features! «",
+        IsNormal = true,
+        Visible = true,
+        LockSize = true,
+    })
+end
+
+return misctabs
