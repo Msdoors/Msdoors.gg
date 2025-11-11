bfd1260f8f3c8ae0fc6d13ba5f31c5edc621420f
Msdoors <msdoors.gg@gmail.com>
2025-11-11 19:29:35 -0300

    TRANSLATORS

--- misc/main/tabs/info.lua
+++ misc/main/tabs/info.lua
@@ -1,26 +1,53 @@
-local misctabs = {} -- SOGMA EZ
+local misctabs = {}
+local HttpService = game:GetService("HttpService")
 
--- CREDITS, TRANSLATORS, ETC...
 function misctabs:AddMiscTab(Tab)
     local GroupCredits = Tab:AddLeftGroupbox(shared.translationapi:getTranslate("tab_Credits", "Desenvolvedores"))
     local GroupContributors = Tab:AddLeftGroupbox(shared.translationapi:getTranslate("tab_Credits_Contributors", "Contribuidores"))
     local GroupTranslators = Tab:AddRightGroupbox(shared.translationapi:getTranslate("tab_Credits_Translators", "Tradutores"))
 
-    --[[ DEVS ]]--
-    local DevsCreditsRhyan = GroupCredits:AddLabel('[Rhyan57] | OWNER ', true)
-    local DevsCreditsSeek = GroupCredits:AddLabel('[SeekAlegriaFla] | SUB-OWNER ', true)
+    local DevsCreditsRhyan = GroupCredits:AddLabel('<font color="rgb(255, 87, 87)">[Rhyan57] | OWNER</font>', true)
+    local DevsCreditsSeek = GroupCredits:AddLabel('<font color="rgb(255, 165, 0)">[SeekAlegriaFla] | SUB-OWNER</font>', true)
 
-    --[[ CONTRIBUTORS ]]--
-    local EspecialCreditsSeikoso = GroupContributors:AddLabel('[Seikoso] | It helped me with tips and to not give up on this script.', true)
-    local EspecialCreditsJacob = GroupContributors:AddLabel('[Jacob] | He is a great person and helped me with tips and optimizing some of the script functions.', true)
-    local EspecialCreditsKardinCat = GroupContributors:AddLabel('[TheHunterSolo1] | This guy made several changes to the Msdoors code and optimized it a lot, and also gave me several tips.', true)
-    local EspecialCreditsG1gaBac0n = GroupContributors:AddLabel('[G1ga Bac0n] | for a while he was my Tester and helped me find bugs in the script.', true)
+    local EspecialCreditsSeikoso = GroupContributors:AddLabel('<font color="rgb(138, 43, 226)">[Seikoso]</font> | It helped me with tips and to not give up on this script.', true)
+    local EspecialCreditsJacob = GroupContributors:AddLabel('<font color="rgb(30, 144, 255)">[Jacob]</font> | He is a great person and helped me with tips and optimizing some of the script functions.', true)
+    local EspecialCreditsKardinCat = GroupContributors:AddLabel('<font color="rgb(255, 20, 147)">[TheHunterSolo1]</font> | This guy made several changes to the Msdoors code and optimized it a lot, and also gave me several tips.', true)
+    local EspecialCreditsG1gaBac0n = GroupContributors:AddLabel('<font color="rgb(50, 205, 50)">[G1ga Bac0n]</font> | for a while he was my Tester and helped me find bugs in the script.', true)
 
-    --[[ TRANSLATORS ]]--
-    -- SOOON
+    local success, result = pcall(function()
+        return game:HttpGet("https://raw.githubusercontent.com/msdoors-gg/msdoors-translations/refs/heads/main/translators.json")
+    end)
+
+    if success and result then
+        local translators = HttpService:JSONDecode(result)
+        local isFirst = true
+        
+        for discordId, data in pairs(translators) do
+            if not isFirst then
+                GroupTranslators:AddDivider()
+            end
+            isFirst = false
+            
+            local colors = {
+                "rgb(73, 230, 133)",
+                "rgb(255, 107, 107)",
+                "rgb(86, 204, 242)",
+                "rgb(255, 184, 77)",
+                "rgb(199, 125, 255)",
+                "rgb(255, 121, 198)"
+            }
+            local colorIndex = (tonumber(discordId:sub(1, 3)) or 0) % #colors + 1
+            local color = colors[colorIndex]
+            
+            GroupTranslators:AddLabel('<font color="' .. color .. '">' .. data.username .. '</font>', true)
+            
+            for langCode, langData in pairs(data.languages) do
+                GroupTranslators:AddLabel('  • ' .. langData.displayName, true)
+            end
+        end
+    end
 end
 
--- SUMMARY UPDATE: DOORS
 function misctabs:AddWarnTabDoors(Tab)
     Tab:UpdateWarningBox({
         Title = shared.translationapi:getTranslate("Update Summary", "Resumo de Atualizações"),
@@ -31,7 +58,7 @@ function misctabs:AddWarnTabDoors(Tab)
         LockSize = true,
     })
 end
--- SUMMARY UPDATE: NATURAL DISASTER
+
 function misctabs:AddWarnTabNaturalDisaster(Tab)
     Tab:UpdateWarningBox({
         Title = shared.translationapi:getTranslate("Update Summary", "Resumo de Atualizações"),
